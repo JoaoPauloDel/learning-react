fad1d77a342c7b1e1ca821cdc3c94b55e9cfaee8
JoaoPauloDel <joaopaulodelvecchio@gmail.com>
2026-03-12 10:47:28 -0300

    Rename BoasBindas->BoasVindas, fix onClick
    Rename component file BoasBindas.jsx to BoasVindas.jsx and update imports. Update App.jsx to import BoasVindas and Box, add several BoasVindas usages (with nome/genero props) and wrap sample content in Box. Fix Butao confirm onClick to pass a function (() => alert(...)) instead of invoking alert during render. Add new tailwind.config.js placeholder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import Butao from "./components/Butao";
+import BoasVindas from "./components/BoasVindas";
+import Box from "./components/Box";
 
 const App = () => {
   return (
-    <>  
+    <>
+      <div>
+
+        <BoasVindas nome="joao" genero="m" />
+        <BoasVindas nome="jonas" />
+        <BoasVindas nome="heitor" genero="m" />
+
+        <Box>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quia non enim explicabo vel minus praesentium mollitia numquam, quae ipsa voluptas, vero debitis quas ducimus fugit est dignissimos modi nam.
+            </p>
+          </div>
+        </Box>
+      </div>
+
       <div className="flex gap-4">
         <Butao
           texto="Confirmar"
           className={"bg-green-500 rounded"}
-          onClick={alert("confirmou")}
-        />
+          onClick={() => alert("confirmou")} />
         <Butao
           className="font-bold text-white rounded bg-red-500"
           texto="Cancelar"
